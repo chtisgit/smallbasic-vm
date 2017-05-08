72e5000e59ef2c569cebadf7eacd1a14255b0904
Christian Fiedler <fdlr.christian@gmail.com>
2017-05-08 10:49:54 +0200

    Added additional semantic parsing & output

--- assembler/sb_opcodes.h
+++ assembler/sb_opcodes.h
@@ -1,14 +1,46 @@
 #pragma once
 
 #include <map>
+
+#include <cassert>
 #include <cstdint>
 
-std::map<std::string, uint8_t> mnemonic_table = {
-	{"copy", 0},
-	{"movi", 1},
-	{"jmp", 2},
-	{"push", 3},
-	{"pushi", 4}
+enum OperandType{
+	OT_NO,          // no operand
+	OT_REGISTER,
+	OT_IMM16,
+	OT_IMM32
+};
+
+struct Opcode{
+	const uint8_t code;
+	const unsigned operands;
+	const OperandType optype1, optype2;
+
+	Opcode(uint8_t c, unsigned o, OperandType o1, OperandType o2)
+		: code(c), operands(o), optype1(o1), optype2(o2)
+	{
+		// opcodes can have a minimum of zero and
+		// a maximum of 2 operands
+		assert(o >= 0 && o <= 2);
+
+		// simple checks
+		assert(o != 0 || (o1 == OT_NO && o2 == OT_NO));
+		assert(o != 1 || (o1 != OT_NO && o2 == OT_NO));
+		assert(o != 2 || (o1 != OT_NO && o2 != OT_NO));
+
+		// IMM32 values only in o1
+		assert(o2 != OT_IMM32);
+		assert(o1 != OT_IMM32 || o2 == OT_NO);
+	}
+};
+
+std::map<std::string, Opcode> mnemonic_table = {
+	{"copy", {0, 2, OT_REGISTER, OT_REGISTER}},
+	{"movi", {1, 2, OT_REGISTER, OT_IMM16}},
+	{"jmp", {2, 1, OT_IMM32, OT_NO}},
+	{"push", {3, 1, OT_REGISTER, OT_NO}},
+	{"pushi", {4, 1, OT_IMM32, OT_NO}}
 };
 
 
--- assembler/sbas.cpp
+++ assembler/sbas.cpp
@@ -1,6 +1,7 @@
 #include "sb_opcodes.h"
 
 #include <algorithm>
+#include <fstream>
 #include <iostream>
 #include <iterator>
 #include <sstream>
@@ -12,14 +13,7 @@
 
 using namespace std;
 
-inline auto skip(char *s, char x) -> char*
-{
-	assert(x != '\0');
-	while(*s == x) s++;
-	return s;
-}
-
-auto main() -> int
+auto assemble(istream& in, ostream& out) -> int
 {
 	constexpr int LINE_LEN = 200;
 	char line[LINE_LEN];
@@ -28,17 +22,13 @@ auto main() -> int
 	for(;;){
 
 		line_n++;
-		cin.getline(line, LINE_LEN);
-		if(cin.eof()){
+		in.getline(line, LINE_LEN);
+		if(in.eof()){
 			break;
 		}
-		if(cin.fail()){
+		if(in.fail()){
 			cerr << "error: line " << line_n << " too long!" << endl;
-			do{
-				cin.clear();
-				cin.getline(line, LINE_LEN);
-			}while(cin.fail());
-			continue;
+			return 1;
 		}
 		istringstream st{ string(line) };
 		vector<string> tok;
@@ -51,9 +41,60 @@ auto main() -> int
 			/* empty line */
 			continue;
 		}
-		cout << "mnemonic: '" << tok[0] << "' opcode="
-		     << int(mnemonic_table.at(tok[0])) << endl;
+		auto& op = mnemonic_table.at(tok[0]);
 
+		assert(tok.size() >= 1);
+		if(op.operands != tok.size() - 1){
+			cerr << "error: mnemonic " << tok[0]
+			     << " takes " << op.operands
+			     << " operand(s)" << endl;
+			return 1;
+		}
+
+		out << op.code;
+
+		// Debugging
+		cerr << "mnemonic: '" << tok[0] << "' opcode="
+		     << int(mnemonic_table.at(tok[0]).code) << endl;
+		
 	}
 	return 0;
+}
+
+auto help(const char *app) -> void
+{
+	cerr << "Usage: " << app << "[input file] [output file]" << endl
+	     << endl << "default input file: standard input" << endl
+	     << "default output file: a.out" << endl;
+}
+
+auto main(int argc, char **argv) -> int
+{
+	int rv;
+	switch(argc){
+	case 1:
+		{
+		ofstream out("a.out");
+		rv = assemble(cin, out);
+		}
+		break;
+	case 2:
+		{
+		ifstream in(argv[1]);
+		ofstream out("a.out");
+		rv = assemble(in, out);
+		}
+		break;
+	case 3:
+		{
+		ifstream in(argv[1]);
+		ofstream out(argv[2]);
+		rv = assemble(in, out);
+		}
+		break;
+	default:
+		help(argv[0]);
+		return 3;
+	}
+	return rv;
 }
