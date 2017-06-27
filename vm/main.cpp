1d2a3f232d6fdeed0d1994ebd3bd03a569858c19
Christian Fiedler <fdlr.christian@gmail.com>
2017-06-27 21:35:02 +0200

    removed unneccessary scopes and vars in main.cpp

--- vm/main.cpp
+++ vm/main.cpp
@@ -35,34 +35,22 @@ auto run(CodeFile& file) -> void
 
 	dispatch(0);
 	// VM operations:
-op_copy:{
-	// TODO: mov operation
-	//printf("copy\n");
-	int reg1 = state.decode1();
-	int reg2 = state.decode2();
-	state.registers[reg1] = state.registers[reg2];	
-	}
+op_copy:
+	state.registers[state.decode1()] = state.registers[state.decode2()];
 	next_instr;
-op_movi:{
-	// TODO: add operation
-	//printf("movi\n");
-	int reg1 = state.decode1();
-	int reg2 = state.decode2();
-	state.registers[reg1] = reg2;
-	}
+op_movi:
+	state.registers[state.decode1()] = state.decode2();
 	next_instr;
-op_jmp:{
+op_jmp:
 	state.jump(state.decode_imm32());
-	}
 	dispatch(0);
 op_push:{
 	int reg1 = state.decode1();
 	state.stack.push_back(state.registers[reg1]);
 	}
 	next_instr;
-op_pushi:{
+op_pushi:
 	state.stack.push_back(state.decode_imm32());
-	}
 	next_instr;
 op_add:{
 	int reg1 = state.decode1();
@@ -107,17 +95,14 @@ op_call:{
 	state.call(reg1);
 	}
 	dispatch(0);
-op_ret:{
+op_ret:
 	if(!state.ret()) return;
-	}	
 	dispatch(0);
-op_jc:{
+op_jc:
 	state.conditional_reljump(state.decode_imm32(), 5);
-	}
 	dispatch(0);
-op_jnc:{
+op_jnc:
 	state.conditional_reljump(5, state.decode_imm32());
-	}
 	dispatch(0);
 op_lstr:{
 	if(state.stack.empty())
@@ -125,53 +110,51 @@ op_lstr:{
 	const char *str = reinterpret_cast<const char*>(state.file.code()) + state.stack.back().getIntVal();
 	state.registers[state.decode1()]= std::string(str);
 	state.stack.pop_back();
-}
-next_instr;
+	}
+	next_instr;
 op_equ:{
 	int reg1_content = state.registers[state.decode1()].getIntVal();
 	int reg2_content = state.registers[state.decode2()].getIntVal();
 	state.set_cond(reg1_content == reg2_content);
-}
-next_instr;
+	}
+	next_instr;
 ops_neq:{
 	int reg1_content = state.registers[state.decode1()].getIntVal();
 	int reg2_content = state.registers[state.decode2()].getIntVal();
 	state.set_cond(reg1_content != reg2_content);
-}
-next_instr;
+	}
+	next_instr;
 op_grt:{
 	int reg1_content = state.registers[state.decode1()].getIntVal();
 	int reg2_content = state.registers[state.decode2()].getIntVal();
 	state.set_cond(reg1_content > reg2_content);
-} 
-next_instr;
+	} 
+	next_instr;
 op_lrt:{
 	int reg1_content = state.registers[state.decode1()].getIntVal();
 	int reg2_content = state.registers[state.decode2()].getIntVal();
 	state.set_cond(reg1_content < reg2_content);
-}
-next_instr;
+	}
+	next_instr;
 op_geq:{
 	int reg1_content = state.registers[state.decode1()].getIntVal();
 	int reg2_content = state.registers[state.decode2()].getIntVal();
 	state.set_cond(reg1_content >= reg2_content);
-}
-next_instr;
+	}
+	next_instr;
 op_leq:{
 	int reg1_content = state.registers[state.decode1()].getIntVal();
 	int reg2_content = state.registers[state.decode2()].getIntVal();
 	state.set_cond(reg1_content <= reg2_content);
-}
-next_instr;
-op_ccat:{
+	}
+	next_instr;
+op_ccat:
 	state.registers[state.decode1()] = state.registers[state.decode1()].getStringVal() 
 										+ state.registers[state.decode2()].getStringVal();
-}
-next_instr;
-op_len:{
+	next_instr;
+op_len:
 	state.registers[state.decode1()] = state.registers[state.decode2()].length();
-}
-next_instr;
+	next_instr;
 op_slice:{
 	std::string s = state.registers[state.decode1()].getStringVal();
 	int i =  state.registers[state.decode2()].getIntVal();
@@ -183,33 +166,30 @@ op_slice:{
 										s.substr(0, i):
 										s.substr(0, s.length() + i-1);				
 	}
-next_instr;
+	next_instr;
 op_read:{
 	int i = state.stack.back().getIntVal();
 	state.registers[state.decode1()] = state.registers[state.decode2()][i];
  state.stack.pop_back();
-}
-next_instr;
+	}
+	next_instr;
 op_write:{
 	int i = state.stack.back().getIntVal();
 	state.registers[state.decode1()][i] = state.registers[state.decode2()];
 	state.stack.pop_back();
-}
-next_instr;
-op_dim:{
+	}
+	next_instr;
+op_dim:
 	state.registers[state.decode1()][state.registers[state.decode2()].getIntVal()] = 0.0;
-}
-next_instr;
-op_zero:{
+	next_instr;
+op_zero:
 	state.registers[state.decode1()] = 0;
 	state.registers[state.decode2()] = 0;
-}
-next_instr;
-op_estr:{
+	next_instr;
+op_estr:
 	state.registers[state.decode1()].getStringVal().clear();
 	state.registers[state.decode2()].getStringVal().clear();
-}
-next_instr;
+	next_instr;
 op_obj:{
 	auto safety_check = state.file.code();
 	auto obj = state.decode1();
@@ -220,22 +200,24 @@ op_obj:{
 	assert(ret != 0);
 	/* the called instruction must not change the instruction pointer */
 	assert(state.file.code() == safety_check);
-}
-next_instr;
+	}
+	next_instr;
+
 #ifdef DEBUG
 reg_debug:{
 	int reg1 = state.decode1();
 	int reg2 = state.decode2();
 	printf("reg: %d content: %d\n", reg1, state.registers[reg1].getIntVal());	
 	printf("reg: %d content: %d\n", reg2, state.registers[reg2].getIntVal());
-}
-next_instr;
+	}
+	next_instr;
 str_debug:{
 	int reg = state.decode1();	
 	printf("reg: %d content: %s\n", reg, state.registers[reg].getStringVal().c_str());
-}
-next_instr;
-#endif
+	}
+	next_instr;
+#endif // DEBUG
+
 op_notimplemented:{
 	fprintf(stderr, "error: opcode %d not implemented!\n",
 			int(state.decode()));
