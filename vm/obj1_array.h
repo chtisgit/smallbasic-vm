ec46c6c89c94235d2489bc0afb88f01fe78ee99d
Christian Fiedler <fdlr.christian@gmail.com>
2017-10-31 13:12:59 +0100

    Forgot to add obj1...h obj2...h

---
+++ vm/obj1_array.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "value.h"
+
+namespace ObjArray{
+
+// Functions
+
+int ContainsIndex(VMState& state){ return 0; }
+int ContainsValue(VMState& state){ return 0; }
+int GetAllIndices(VMState& state){ return 0; }
+int GetItemCount(VMState& state){ return 0; }
+int IsArray(VMState& state){ return 0; }
+int SetValue(VMState& state){ return 0; }
+int GetValue(VMState& state){ return 0; }
+int RemoveValue(VMState& state){ return 0; }
+
+};
+
---
+++ vm/obj2_text.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <cstdio>
+
+#include "value.h"
+
+namespace ObjText{
+
+// Functions
+
+int Append(VMState& state){ return 0; }
+int GetLength(VMState& state){ return 0; }
+int IsSubText(VMState& state){ return 0; }
+int EndsWith(VMState& state){ return 0; }
+int StartsWith(VMState& state){ return 0; }
+int GetSubText(VMState& state){ return 0; }
+int GetSubTextToEnd(VMState& state){ return 0; }
+int GetIndexOf(VMState& state){ return 0; }
+int ConvertToLowerCase(VMState& state){ return 0; }
+int ConvertToUpperCase(VMState& state){ return 0; }
+int GetCharacter(VMState& state){ return 0; }
+int GetCharacterCode(VMState& state){ return 0; }
+
+}
